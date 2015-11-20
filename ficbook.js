c63c64fd0f7e19e8fd34aa3562829f39f116772a
dtestyk <jj1kn6rum@shitmail.me>
2015-11-20 08:03:35 +0300

    add main files

---
+++ ficbook.js
@@ -0,0 +1,17 @@
+var delete_all_personal_messages_button = document.querySelector('.clean_pm')
+if(delete_all_personal_messages_button){
+  delete_all_personal_messages_button.style.display='none'
+  console.log('delete_all_personal_messages_button was hided.')
+}
+
+//var menu_shop_tab = document.querySelector('.menu td:nth-child(6)')
+//if(menu_shop_tab){
+//  menu_shop_tab.style.display='none'
+//  console.log('menu_shop_tab was hided.')
+//}
+
+//var yandex_ad_bar = document.querySelector('#yandex_hor')
+//if(yandex_ad_bar){
+//  yandex_ad_bar.style.display='none'
+//  console.log('yandex_ad_bar was hided.')
+//}
---
+++ seasonvar.js
@@ -0,0 +1,34 @@
+console.log("personal extension content script for seasonvar loaded.")
+var left_banner = document.querySelector('.td-for-left-block')
+if(left_banner){
+  //left_banner.parentNode.removeChild(bottom_right_banner)
+  //console.log("bottom_right_banner was removed.")
+  left_banner.style.display='none'
+  console.log('left_banner hided.')
+}
+
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    if (mutation.addedNodes) {
+      for (var i = 0; i < mutation.addedNodes.length; i++) {
+        var appended_node = mutation.addedNodes[i];
+        if(!appended_node) continue
+        if(!appended_node.querySelector) continue
+        
+        var bottom_right_banner = appended_node.querySelector('#b_sl_51')
+        if(bottom_right_banner){
+          bottom_right_banner.parentNode.removeChild(bottom_right_banner)
+          console.log("bottom_right_banner removed.")
+          observer.disconnect()
+        }
+      }
+    }
+  });
+});
+
+observer.observe(document, {
+  childList: true,
+  attributes: false,
+  characterData: false,
+  subtree: true
+});
